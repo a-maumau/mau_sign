cdd3e4d41758b8e07f643957f37309a8bc92e699
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2017-10-08 02:00:08 +0900

    new

--- mau_sign.c
+++ mau_sign.c
@@ -1,57 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-char mau_sign[] = {
-    {"                                                    "}.
-    {"                M{                                  "}.
-    {"                M|                                  "}.
-    {"                Mb                                  "}.
-    {"                (M.     ....                        "}.
-    {"                 Mb.JNMM\"\"\"                   "}.
-    {"            ...gMMM\"`                             "}.
-    {"       ..+MM\"\"!  .M;                     +N`    "}.
-    {"   ..MMB\"`        Mb                   .dMF       "}.
-    {"   ?=             JN        ..        .MMD          "}.
-    {"                  ,M|   .JMM\"=       .MM^         "}.
-    {"                   Mb.MM\"!          .MM'          "}.
-    {"                ..MM#^             .M@              "}.
-    {"           ..+M#\"^ JN              _!             "}.
-    {"        (MH\"=`     -M.                            "}.
-    {"                   .M~                              "}.
-    {"                   .M~                              "}.
-    {"                   .M~   ...JgMMMMMMMMMMMNa..       "}.
-    {"                   .M+JMM#\"\"!             .TMp  "}.
-    {"                 ..MMD!                     ,Mb     "}.
-    {"              ..MM\"(M~                       ,Mp  "}.
-    {"            .M#\"`  (M                         ?M, "}.
-    {"          (M\"`     MF                          ?M,"}.
-    {"        .MD       .M\\                           Mb  "}.
-    {"      .d#!        d#                            dN  "}.
-    {"     .M3         .M^                            JM  "}.
-    {"     (M         .MF                             JM  "}.
-    {"     (M.       .MF                              JM  "}.
-    {"      UN,    ..MD                               d#  "}.
-    {"       (WMMMM\"\"                                .MF"}.
-    {"                                               JM` "}.
-    {"                                              .M\\ "}.
-    {"                                             .MF    "}.
-    {"                                             d#     "}.
-    {"                                            -M'     "}.
-    {"                                           .Mt      "}.
-    {"                                          .M$       "}.
-    {"                                        .dM!        "}.
-    {"                                       .MD          "},
-    {"                                     .M#^           "},
-    {"                                  .JM#^             "}.
-    {"                               .dMB=                "},
-    {"                                                    "}
-}
+char mau_sign[] = "                                                    \n                M{                                  \n                M|                                  \n                Mb                                  \n                (M.     ....                        \n                 Mb.JNMM\"\"\"                         \n            ...gMMM\"`                               \n       ..+MM\"\"!  .M;                     +N`        \n   ..MMB\"`        Mb                   .dMF        \n   ?=             JN        ..        .MMD          \n                  ,M|   .JMM\"=       .MM^           \n                   Mb.MM\"!          .MM'            \n                ..MM#^             .M@              \n           ..+M#\"^ JN              _!               \n        (MH\"=`     -M.                              \n                   .M~                              \n                   .M~                              \n                   .M~   ...JgMMMMMMMMMMMNa..       \n                   .M+JMM#\"\"!             .TMp      \n                 ..MMD!                     ,Mb     \n              ..MM\"(M~                       ,Mp    \n            .M#\"`  (M                         ?M,   \n          (M\"`     MF                          ?M,  \n        .MD       .M\\                           Mb  \n      .d#!        d#                            dN  \n     .M3         .M^                            JM  \n     (M         .MF                             JM  \n     (M.       .MF                              JM  \n      UN,    ..MD                               d#  \n       (WMMMM\"\"                                .MF  \n                                               JM`  \n                                              .M\\   \n                                             .MF    \n                                             d#     \n                                            -M'     \n                                           .Mt      \n                                          .M$       \n                                        .dM!        \n                                       .MD          \n                                     .M#^           \n                                  .JM#^             \n                               .dMB=                \n                                                    ";
 int main()
 {
-    int i;
-    line = sizeof(mau_sign);
-
-    for(i=0; i < line; i++) print("%s", mau_sign[i]);
-
+    printf("%s\n", mau_sign);
     return 0;
 }
